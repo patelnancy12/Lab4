85f4bb7357659a46ab12d147505753213658a562
patelnancy12 <Nancy.Patel@dal.ca>
2019-01-30 19:01:53 +0000

    Task Done

--- l4task1.py
+++ l4task1.py
@@ -1,3 +1,14 @@
 import string 
 print(string.punctuation)
 
+def display_string():
+
+  str1 = []
+  fout = open("file.txt",'r')
+
+  for line in fout:
+    str2 = line.lower().strip(string.punctuation + string.whitespace)
+    str1 += str2
+  return str1
+
+print(display_string())
