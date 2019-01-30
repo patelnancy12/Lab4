44cb8389cf0e73364c3b255bfc9633c96dc53a37
patelnancy12 <Nancy.Patel@dal.ca>
2019-01-30 19:07:46 +0000

    Task Done

---
+++ l4task2.py
@@ -0,0 +1,12 @@
+import string 
+
+def display_string():
+  str1 = []
+  fout = open("file.txt",'r')
+  for line in fout:
+    str2 = line.lower().strip(string.punctuation + string.whitespace)
+    str1 += str2
+  return str1
+
+
+print(display_string())
