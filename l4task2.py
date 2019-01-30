03bffe69dc0b32c6f1a6fc68366b94270c0a2265
patelnancy12 <Nancy.Patel@dal.ca>
2019-01-30 19:30:32 +0000

    Task Done

--- l4task2.py
+++ l4task2.py
@@ -1,12 +1,23 @@
 import string 
 
-def display_string():
+def count_string():
   str1 = []
-  fout = open("file.txt",'r')
-  for line in fout:
+  fout = open("file1.txt",'r')
+  r = fout.readlines()
+
+  for line in r:
     str2 = line.lower().strip(string.punctuation + string.whitespace)
     str1 += str2
-  return str1
+
+  total = {}
+  for word in  str1:
+    total[word] = total.get(word, 0) + 1
+
+  words_t = len(str1)
+  words_d = len(total.keys())
+
+  print("Total Words : " + str(words_d))
+  print("Different Words :" + str(words_d))
 
 
-print(display_string())
+print(count_string())
