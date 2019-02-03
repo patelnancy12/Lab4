3c851476bc6339d3c7f682d6f746430641ce461e
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-03 16:54:47 +0000

    task done

--- l4task4.py
+++ l4task4.py
@@ -1,2 +1,19 @@
-# Program to count words in file and replace and perform string operations
+# Program to count words and perform various other strig operations...
 
+import string
+
+def frequent_words():
+
+  str1 = []
+  fout = open("file1.txt",'r')
+  r = fout.readlines()
+  for l in fout.readlines():
+    l = l.strip()
+    w = l.split()
+
+  for line in r:
+    str2 = line.lower().strip(string.punctuation + string.whitespace)
+    str1 += str2
+
+
+print(frequent_words())
